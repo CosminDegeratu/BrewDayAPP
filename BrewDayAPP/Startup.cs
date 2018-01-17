553e5d412017f749baeda2b2063f7cddf2aa73b9
Degeratu Cosmin <degeratucosmin@gmail.com>
2018-01-17 20:11:07 +0100

    Aggiungere i file di progetto.

---
+++ BrewDayAPP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BrewDayAPP.Startup))]
+namespace BrewDayAPP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
