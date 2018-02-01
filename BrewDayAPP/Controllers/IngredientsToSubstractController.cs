2b24ec81e7121c6fa1c7b950ce58eb724c4fd1f4
Degeratu Cosmin <degeratucosmin@gmail.com>
2018-02-01 21:18:34 +0100

    sistemata chiamata javascript per il caricamento della pagina

--- BrewDayAPP/Controllers/IngredientsToSubstractController.cs
+++ BrewDayAPP/Controllers/IngredientsToSubstractController.cs
@@ -8,33 +8,17 @@ namespace BrewDayAPP.Controllers
 {
     public class IngredientsToSubstractController : Controller
     {
-        // GET: IngredietToSubstract
+        // GET: IngredientToSubstract
         public ActionResult _Index(int? recipiesID, int batchSize)
         {
             BrewDayDBEntities db = new BrewDayDBEntities();
-
-            //const string strApplicationCode = "APP";
-            //int userid = Convert.ToInt32(HttpContext.Current.Session["sysUserId"]);
-            //var applist = GetAvailableItems(userid, strApplicationCode);
-
             string query = "select r.ID as IdRecipie, i.ID as IdIngredient, i.[Description], i.UnitMeasure, i.Quantity, ir.AbsolutQuantity, " + batchSize + " as [Batchsize], (ir.AbsolutQuantity * " + batchSize + ") as QuantityToSubstract, (i.Quantity - (ir.AbsolutQuantity * " + batchSize + ")) as QuantityAfterBrew, i.Threshold" +
                                                                         " from Ingredients as i inner join" +
                                                                         " IngredientRecipe ir on i.ID = ir.IdIngredients inner join" +
-                                                                        " Recipies as r on ir.IdRecipes = r.ID inner join" +
-                                                                        " Brews as b on r.ID = b.IdRecipies" +
+                                                                        " Recipies as r on ir.IdRecipes = r.ID " +
                                                                         " where r.ID =" + recipiesID +
                                                                         " group by r.ID, i.ID, i.[Description], i.UnitMeasure, i.Quantity, ir.AbsolutQuantity, i.Threshold";
             IEnumerable<IngredientToSubstract> data = db.Database.SqlQuery<IngredientToSubstract>(query);
-
-
-            //BrewDayDBEntities db = new BrewDayDBEntities();
-            //IList<IngredientsToSubstract> = db.Database.ExecuteSqlCommand("select r.ID as IdRecipie, i.ID as IdIngredient, i.[Description], i.UnitMeasure, i.Quantity, ir.AbsolutQuantity, b.[Batchsize], (ir.AbsolutQuantity* b.[BatchSize]) as QuantityToSubstract, (i.Quantity - (ir.AbsolutQuantity* b.[BatchSize])) as QuantityAfterBrew, i.Threshold" +
-            //                                                        " from Ingredients as i inner join" +
-            //                                                        " IngredientRecipe ir on i.ID = ir.IdIngredients inner join" +
-            //                                                        " Recipies as r on ir.IdRecipes = r.ID inner join" +
-            //                                                        " Brews as b on r.ID = b.IdRecipies" +
-            //                                                        " where r.ID =" + recipiesID + 
-            //                                                        " group by r.ID, i.ID, i.[Description], i.UnitMeasure, i.Quantity, ir.AbsolutQuantity, b.[Batchsize], i.Threshold");
             return PartialView(data.ToList());
         }
 
