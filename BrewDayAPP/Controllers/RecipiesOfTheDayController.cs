5965591c7be63e6c645d1b37fe9d936a581377e3
Degeratu Cosmin <degeratucosmin@gmail.com>
2018-02-04 04:51:52 +0100

    Aggiunto brews of the day con tutta la relativa gestione, controller, view model e sp che carica la tabella e query che seleziona con chiamato action link in brew create html

---
+++ BrewDayAPP/Controllers/RecipiesOfTheDayController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrewDayAPP.Controllers
+{
+    public class RecipiesOfTheDayController : Controller
+    {
+        // GET: RecipiesOfTheDay
+        public ActionResult _Index()
+        {
+            BrewDayDBEntities db = new BrewDayDBEntities();
+            //recupera dal model parametri che mi servono per la spUpdateQuantityIngredients
+            var userID = User.Identity.GetUserId();
+            var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+
+            if (User.Identity.GetUserName().Equals(ConfigurationManager.AppSettings["SuperUser"]))
+            {
+                //lancia la spRecuperaRecipiesOfTheDayAll
+                var command = new SqlCommand("spRecuperaRecipiesOfTheDayAll", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            else
+            {
+                //lancia la spUpdateQuantityIngredients con parametro userID
+                var command = new SqlCommand("spRecuperaRecipiesOfTheDay", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@userId", userID);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            var query = "select ID,[Description], [batchSize],Rate from RecipiesofTheDay";
+            IEnumerable<RecipiesOfTheDay> data = db.Database.SqlQuery<RecipiesOfTheDay>(query);
+            return PartialView(data.ToList());
+        }
+    }
+}
---
+++ BrewDayAPP/Models/RecipiesOfTheDayModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BrewDayAPP
+{
+    public class RecipiesOfTheDay
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public Nullable<double> batchSize { get; set; }
+        public int Rate { get; set; }
+    }
+}
